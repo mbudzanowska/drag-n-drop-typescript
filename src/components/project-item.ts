d42a41cc95967afd2950ede92cec23915f8c2ffb
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-08 15:02:44 +0200

    adding bundling with webpack

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from "../models/drag-drop.js";
-import { Project } from "../models/project.js";
-import Cmp from "./base-component.js";
-import { autobind } from "../decorators/autobind.js";
+import { Draggable } from "../models/drag-drop";
+import { Project } from "../models/project";
+import Cmp from "./base-component";
+import { autobind } from "../decorators/autobind";
 
 export class ProjectItem
   extends Cmp<HTMLUListElement, HTMLLIElement>
