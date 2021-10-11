3f9447414d13c46b82188b33195d59f68b081ae8
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-11 11:40:51 +0200

    declare var global

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import _ from "lodash";
-
 console.log(_.shuffle([1,2,3]));
+
+declare var GLOBAL: string;
+console.log(GLOBAL);
 
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
