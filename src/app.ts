214181cbff67c101d4840ebe9746c4b84c7ec79d
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-11 12:02:33 +0200

    change module config to commonjs, add class-transformer and reflect-metadata that helps transform jsons into instances of specific classes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,43 @@
+
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+
 import _ from "lodash";
-console.log(_.shuffle([1,2,3]));
+
+console.log(_.shuffle([1, 2, 3]));
 
 declare var GLOBAL: string;
 console.log(GLOBAL);
 
-import { ProjectInput } from './components/project-input';
-import { ProjectList } from './components/project-list';
+import { Product } from "./product.model";
+
+const p1 = new Product("A book", 12.99);
+console.log(p1.getInformation());
+
+///////
+
+// assuming these comes from the backend as a plain json
+const products = [
+  { title: "A carpet", price: 2.99 },
+  { title: "A book", price: 12.99 },
+];
+
+// manually transforming to the instances
+const loadedProducts = products.map((prod) => {
+  return new Product(prod.title, prod.price);
+});
+
+console.log(loadedProducts);
+
+
+
+const loadedProducts_better = plainToClass(Product, products);
+console.log(loadedProducts_better);
+///////////////////
+
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
 
 new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+new ProjectList("active");
+new ProjectList("finished");
