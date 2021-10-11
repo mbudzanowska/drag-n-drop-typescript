9b30b505a2eeadbb96c4387b43cf1c64fbf56ab8
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-11 12:11:21 +0200

    add class-validator dependency, simple validation example

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
-
-import 'reflect-metadata';
-import { plainToClass } from 'class-transformer';
-
+// import @types
 import _ from "lodash";
-
 console.log(_.shuffle([1, 2, 3]));
 
+// declare
 declare var GLOBAL: string;
 console.log(GLOBAL);
 
@@ -15,6 +12,9 @@ const p1 = new Product("A book", 12.99);
 console.log(p1.getInformation());
 
 ///////
+
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
 
 // assuming these comes from the backend as a plain json
 const products = [
@@ -29,11 +29,39 @@ const loadedProducts = products.map((prod) => {
 
 console.log(loadedProducts);
 
-
-
 const loadedProducts_better = plainToClass(Product, products);
 console.log(loadedProducts_better);
 ///////////////////
+
+import { IsNotEmpty, IsNumber, IsPositive, validate } from "class-validator";
+
+export class Product2 {
+  @IsNotEmpty()
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+
+  getInformation() {
+    return [this.title, `$${this.price}`];
+  }
+}
+
+const newProd = new Product2("", -5.99);
+validate(newProd).then(errors => {
+  if(errors.length > 0){
+    console.log("validation errors");
+    console.log(errors);
+  }
+  console.log(newProd.getInformation());
+})
+///
 
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
